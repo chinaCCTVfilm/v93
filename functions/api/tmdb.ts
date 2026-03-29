a6d887ac238e907e9ba3f327b9aaedd8080fddf0
chinaCCTVfilm <limingvv789@gmail.com>
2026-03-30 00:26:17 +0800

    deploy 03/30/2026 Mon  0:26:17.64

--- functions/api/tmdb.ts
+++ functions/api/tmdb.ts
@@ -1,25 +1,7 @@
-export const onRequestGet: PagesFunction = async ({ request, env }) => {
-  const url = new URL(request.url);
+export const onRequestGet: PagesFunction = async ({ env }) => {
 
-  const page = url.searchParams.get("page") || "1";
-
-  const apiKey = env.TMDB_API_KEY;
-
-  if (!apiKey) {
-    return new Response(JSON.stringify({
-      error: "Missing TMDB_API_KEY"
-    }), { status: 500 });
-  }
-
-  const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${page}&language=zh-CN&sort_by=popularity.desc`;
-
-  const res = await fetch(apiUrl);
-  const data = await res.text();
-
-  return new Response(data, {
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "public, max-age=86400"
-    }
-  });
+  return new Response(JSON.stringify({
+    hasKey: !!env.TMDB_API_KEY,
+    keyPreview: env.TMDB_API_KEY ? env.TMDB_API_KEY.slice(0,5) : null
+  }));
 };
