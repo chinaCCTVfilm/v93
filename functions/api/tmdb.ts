882dc8e326a2d6991eb55fc1781ddaed062ea9fc
chinaCCTVfilm <limingvv789@gmail.com>
2026-03-30 16:07:45 +0800

    deploy 03/30/2026 Mon 16:07:45.61

--- functions/api/tmdb.ts
+++ functions/api/tmdb.ts
@@ -1,7 +1,14 @@
-export const onRequestGet: PagesFunction = async ({ env }) => {
+export const onRequestGet: PagesFunction = async ({ env, request }) => {
+  const url = new URL(request.url);
+  const path = url.pathname + url.search;
 
   return new Response(JSON.stringify({
+    success: true,
     hasKey: !!env.TMDB_API_KEY,
-    keyPreview: env.TMDB_API_KEY ? env.TMDB_API_KEY.slice(0,5) : null
-  }));
+    keyLength: env.TMDB_API_KEY ? env.TMDB_API_KEY.length : 0,
+    message: env.TMDB_API_KEY ? "Key 已加载" : "Key 仍为 undefined",
+    envKeys: Object.keys(env) // 查看所有可用 env
+  }, null, 2), {
+    headers: { "Content-Type": "application/json" }
+  });
 };
